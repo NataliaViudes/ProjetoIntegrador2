859d322ea46a990639c9c5b44289da12d7785872
NataliaViudes <nataliaviudesb@gmail.com>
2026-04-29 14:36:35 -0300

    eta nois

---
+++ back-end/src/main/java/pi2/example/back_end/Modelo/CategoriaAuxilio.java
@@ -0,0 +1,60 @@
+package pi2.example.back_end.Modelo;
+
+import pi2.example.back_end.DAO.CatAuxilioDAOImpl;
+import pi2.example.back_end.db.Conexao;
+
+import java.util.List;
+
+public class CategoriaAuxilio {
+    private Integer id;
+    private String nome;
+
+    public CategoriaAuxilio() {
+    }
+
+    public CategoriaAuxilio(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public CategoriaAuxilio(Integer id) {
+        this.id = id;
+        this.nome = "";
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public CategoriaAuxilio buscarPorId(Integer id, Conexao con) {
+        CatAuxilioDAOImpl dao = new CatAuxilioDAOImpl(con);
+        return dao.get(id);
+    }
+
+    public List<CategoriaAuxilio> buscarTodos(Conexao con) {
+        CatAuxilioDAOImpl dao = new CatAuxilioDAOImpl(con);
+        return dao.getAll();
+    }
+
+    public List<CategoriaAuxilio> buscarComFiltro(String filtro, Conexao con) {
+        CatAuxilioDAOImpl dao = new CatAuxilioDAOImpl(con);
+        return dao.get(filtro);
+    }
+
+    public List<CategoriaAuxilio> buscarPorNome(String nome, Conexao con) {
+        CatAuxilioDAOImpl dao = new CatAuxilioDAOImpl(con);
+        return dao.buscarPorNome(nome);
+    }
+}
