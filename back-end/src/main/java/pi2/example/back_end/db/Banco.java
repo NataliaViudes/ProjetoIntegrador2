534227677d4544b0deb627692f2878cbe9827205
Gabriel-Savoldi <gabrielbaldassarini@gmail.com>
2026-05-14 10:11:17 -0300

    Tentando juntar tudo

--- back-end/src/main/java/pi2/example/back_end/db/Banco.java
+++ back-end/src/main/java/pi2/example/back_end/db/Banco.java
@@ -3,7 +3,7 @@ package pi2.example.back_end.db;
 public class Banco  // classe Singleton
 {
     private static Banco singletom;
-    private static final String URL = "jdbc:postgresql://localhost:5432/SCFV2";
+    private static final String URL = "jdbc:postgresql://localhost:5432/SCFV";
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres123";
     public static Conexao getConexao() {
