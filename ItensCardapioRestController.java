355a8e999b2952b06d7a6b9223a908aaa5a9a790
Nati <nataliaviudesb@gmail.com>
2026-05-24 16:15:07 -0300

    Add files via upload
    tai

---
+++ ItensCardapioRestController.java
@@ -0,0 +1,45 @@
+package pi2.example.back_end.RestController;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pi2.example.back_end.Control.ItensCardapioControl;
+import pi2.example.back_end.Modelo.Alimento;
+import pi2.example.back_end.Modelo.Cardapio;
+import pi2.example.back_end.Modelo.ItensCardapio;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/itens-cardapio")
+public class ItensCardapioRestController {
+
+    private final ItensCardapioControl control = new ItensCardapioControl();
+
+    @PutMapping
+    public ResponseEntity<?> atualizar(@RequestBody ItensCardapio item) {
+        return control.alterar(item);
+    }
+
+    @DeleteMapping("/{idCardapio}/{idAlimento}")
+    public ResponseEntity<?> delete(
+            @PathVariable int idCardapio,
+            @PathVariable int idAlimento) {
+        ItensCardapio item = new ItensCardapio();
+        item.setCardapio(new Cardapio());
+        item.getCardapio().setId(idCardapio);
+
+        item.setAlimento(new Alimento());
+        item.getAlimento().setId(idAlimento);
+
+        return control.delete(item);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> salvarOuAtualizar(@RequestBody ItensCardapio item) {
+        return control.salvarOuAtualizar(item);
+    }
+
+    @GetMapping("/{idCardapio}")
+    public ResponseEntity<?> getByCardapio(@PathVariable int idCardapio) {
+        return control.getByCardapio(idCardapio);
+    }
+}
